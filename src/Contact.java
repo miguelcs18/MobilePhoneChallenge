039a2533e7ce0e07339c1ae7831ebbb9a0c37c7c
miguelcs18 <miguelcs18@gmail.com>
2022-09-10 20:47:54 -0500

    Generate constructor and instantiate objects

--- src/Contact.java
+++ src/Contact.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
+
 public class Contact {
     private String name;
     private String phone_number;
+
+    public Contact(String name, String phone_number) {
+        this.name = name;
+        this.phone_number = phone_number;
+    }
 }
--- src/MobilePhone.java
+++ src/MobilePhone.java
@@ -2,5 +2,7 @@ import java.util.ArrayList;
 
 public class MobilePhone {
     public static void main(String[] args) {
+        ArrayList<Contact> contactsList = new ArrayList<>();
+        Contact contact  = new Contact("Miguel","443133003");
     }
 }
