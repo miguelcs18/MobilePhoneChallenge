5110bf92d8568fb7d6989d99bb1f4d63a8071c59
miguelcs18 <miguelcs18@gmail.com>
2022-09-10 20:48:46 -0500

    Add getters and setters

--- src/Contact.java
+++ src/Contact.java
@@ -8,4 +8,20 @@ public class Contact {
         this.name = name;
         this.phone_number = phone_number;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
 }
--- src/MobilePhone.java
+++ src/MobilePhone.java
@@ -4,5 +4,6 @@ public class MobilePhone {
     public static void main(String[] args) {
         ArrayList<Contact> contactsList = new ArrayList<>();
         Contact contact  = new Contact("Miguel","443133003");
+
     }
 }
