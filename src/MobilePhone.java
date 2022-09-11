d2d06691b4bede3c726317ed53c3cd103b9e8875
miguelcs18 <miguelcs18@gmail.com>
2022-09-10 21:49:14 -0500

    Add some objects to test methods addContact and printContactList

--- src/MobilePhone.java
+++ src/MobilePhone.java
@@ -2,7 +2,13 @@ import java.util.ArrayList;
 
 public class MobilePhone {
     public static void main(String[] args) {
+    Contact contacto = new Contact();
 
+    contacto.addContact("Gaby","4431333003");
+    contacto.addContact("Miguel","4431333003");
+    contacto.addContact("Chapa","1234567890");
+
+    contacto.printContactList();
 
     }
 }
