153849a5c5bc450a90bd5f419ff9d4fbbd0c0c19
miguelcs18 <miguelcs18@gmail.com>
2022-09-10 20:39:12 -0500

    Adding member variables

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,2 +1,6 @@
+import java.util.ArrayList;
+
 public class Contacts {
+    private String name;
+    private String phone_number;
 }
--- src/MobilePhone.java
+++ src/MobilePhone.java
@@ -1,2 +1,5 @@
 public class MobilePhone {
+    public static void main(String[] args) {
+
+    }
 }
